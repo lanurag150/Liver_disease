64d8704b3bf77bf6324f25a2127fd8124edda9d7
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-30 13:01:02 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -73,7 +73,7 @@
       </div>
     </div>
 
-   <section class="ftco-section contact-section ftco-degree-bg" style="height: 1200px" style="background-image: url('{{ url_for('static', filename='images/liver.png') }}');">
+   <section class="ftco-section contact-section ftco-degree-bg" style="height: 1200px";background-color:AntiqueWhite;>
    
       <div class="container col-lg-6" >
           
